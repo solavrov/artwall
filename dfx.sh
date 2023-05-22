e2ed74bca67515baf3ac1c0357c0f7427d5997e0
solavrov <2slavrov@gmail.com>
2023-05-22 12:40:28 +0300

    dfx.sh

---
+++ dfx.sh
@@ -0,0 +1,6 @@
+# Make the script executable with command chmod +x <fileName>
+# Run the script using ./<fileName>
+
+dfx canister status --network ic 4u67j-raaaa-aaaal-qb4bq-cai #backend
+dfx canister status --network ic 4bzoe-qiaaa-aaaal-qb4ca-cai #frontend
+
